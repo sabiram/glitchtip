7a1eacb793dbbb980a32a83f6caa9b6c66c1e8c0
sabiram <54712606+sabiram@users.noreply.github.com>
2024-02-12 22:36:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,34 @@
 #!/bin/bash
-python manage.py migrate
+#python manage.py migrate
+
+#!/usr/bin/env sh
+set -e
+
+SERVER_ROLE="${SERVER_ROLE:-web}"
+HEROKU_DYNO="${DYNO:-no}"
+
+case "$HEROKU_DYNO" in
+    web*) ./manage.py migrate ;;
+    worker*) SERVER_ROLE=worker_with_beat ;;
+esac
+
+case $SERVER_ROLE in
+    web)
+        SCRIPT="./bin/run-uwsgi.sh"
+        ;;
+    worker)
+        SCRIPT="./bin/run-celery.sh"
+        ;;
+    beat)
+        SCRIPT="./bin/run-beat.sh"
+        ;;
+    worker_with_beat)
+        SCRIPT="./bin/run-celery-with-beat.sh"
+        ;;
+    *)
+        echo "Unknown server role provided: $SERVER_ROLE. Should be web|worker|beat."
+        exit 1
+        ;;
+esac
+
+. "$SCRIPT"
